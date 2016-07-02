d3c236c0190143a9a77ff3ec5d635ab99dc5002f
Steve Schaeffer <schaefferlab@client-104-39-74-54.mobility.psu.edu>
2016-07-02 13:22:08 -0400

    Initial Commit

---
+++ APPLES/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APPLES
+//
+//  Created by Steve Schaeffer on 7/2/16.
+//  Copyright © 2016 Zach Fuller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
